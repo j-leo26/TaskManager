b321a6f59f00514965952d07d71f8834b8953c91
j-leo26 <joselchavarro26@gmail.com>
2025-06-16 19:55:09 -0500

    first commit

---
+++ src/test/java/com/JoseChavarro/taskManager/TaskManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.JoseChavarro.taskManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
